c3173f18f3f2936b17a9ec489aa3042b8a7c0c2f
tuji <camifoure@gmail.com>
2025-02-25 06:49:09 +0900

    foront リファ

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -2,6 +2,7 @@
 const config = {
   plugins: {
     tailwindcss: {},
+    autoprefixer: {},  // .jsファイルから追加
   },
 };
 
--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+import typographyPlugin from '@tailwindcss/typography';
+import formsPlugin from '@tailwindcss/forms';
+
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -10,8 +13,17 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: '#1976d2',
+          light: '#42a5f5',
+          dark: '#1565c0'
+        }
+      },
+      fontFamily: {
+        sans: ['var(--font-geist-sans)'],
+        mono: ['var(--font-geist-mono)'],
       },
     },
   },
-  plugins: [],
+  plugins: [formsPlugin, typographyPlugin],
 };
