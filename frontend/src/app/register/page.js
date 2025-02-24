9c8457b6d052bb6a1fd5f40fd700d3c11b34ef6f
tuji <camifoure@gmail.com>
2025-02-25 05:38:18 +0900

    【P】エラー解消　完璧

--- frontend/src/app/register/page.js
+++ frontend/src/app/register/page.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 'use client';  // Client Componentとして明示的に宣言
-=======
-"use client";
->>>>>>> backup-branch
 
 import dynamic from 'next/dynamic'; // dynamic importを使用するためのインポート
 
@@ -11,15 +7,8 @@ const RegistrationForm = dynamic(() => import("../../components/RegistrationForm
 
 export default function RegisterPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
-        <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            アカウント登録
-          </h2>
-        </div>
-        <RegistrationForm /> {/* RegistrationFormコンポーネントを表示 */}
-      </div>
-    </div>
+    <main>
+      <RegistrationForm /> {/* RegistrationFormコンポーネントを表示 */}
+    </main>
   );
 }
